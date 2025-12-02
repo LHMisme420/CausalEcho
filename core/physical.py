09967c8f67e07509ceaf2be83c9b723a65e798c8
LHMisme420 <lhmisme2011@gmail.com>
2025-12-02 12:55:20 -0500

    Create physical.py

---
+++ core/physical.py
@@ -0,0 +1,8 @@
+def analyze_physical(media_path: str) -> float:
+    """
+    Evaluates physical coherence (lighting, shadows, geometry)
+    Returns score 0.0–1.0
+    """
+    # TODO: Replace stub with illumination & motion physics checks
+
+    return 0.72
