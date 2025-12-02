c1530174f73093ddebf96ea89ef4c832f5bc44d2
LHMisme420 <lhmisme2011@gmail.com>
2025-12-02 13:33:30 -0500

    Update and rename fingerprint.py to UTILS fingerprint.py

--- utils/# utils/utils/fingerprint.py
+++
@@ -1,8 +0,0 @@
-# utils/fingerprint.py
-import numpy as np
-
-def extract_sensor_fingerprint(frame):
-    noise = frame.astype(np.float32)
-    blurred = cv2.GaussianBlur(noise, (21,21), 12)
-    noise = noise - blurred
-    return noise.flatten()
